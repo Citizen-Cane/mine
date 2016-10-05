286436e6e1de76d41a076b186da5614d4d308777
Citizen-Cane <Citizen-Cane@users.noreply.github.com>
2016-10-05 20:14:59 +0200

    Added utility methods for running main scripts by class name; using it to start the sample projects (Rakhee, Mine) - this significantly reduces the amount of groovy bootstrap code, plus allows for some administration before and after running a main script

--- src/Mine.groovy
+++ src/Mine.groovy
@@ -3,41 +3,35 @@ setInfos(1, "Mine", "Remastered and extended version of the classic PCM script '
 
 // Well, as the PCM player is a bit to complex to fit into a single groovy script,
 // and because the PCM script code is beyond translation due to its sheer size,
-// let's load some java classes to get the job done
+// let's load teaselib PCMPlayer to get the job done
 	 
-def loader = this.class.classLoader
-if (loader == null)
-{
-	showButton("No class loader, bailing out")
+
+///////////////////////////////////////////////
+// Set the project location
+///////////////////////////////////////////////
+
+def scriptResources = new File('scripts', 'Mine.jar')
+def mainScriptClass = 'teaselib.scripts.mine.Mine'
+
+
+///////////////////////////////////////////////
+// Bootstrap TeaseLib
+///////////////////////////////////////////////
+
+def classLoader = this.class.classLoader
+def teaselibClasspath = new File('lib/TeaseLib/TeaseLib.jar')
+if (!teaselibClasspath.exists()) {
+	showButton(teaselibClasspath.getAbsolutePath() + " not found - bailing out")
 }
+classLoader.addURL(teaselibClasspath.toURI().toURL())
 
-def tljar = new File('lib/TeaseLib/TeaseLib.jar')
-if (!tljar.exists())
-{
-	showButton(tljar.getAbsolutePath() + " not found - bailing out")
-}
-loader.addURL(tljar.toURI().toURL())
 
-def minejar = new File('scripts/Mine.jar')
-if (!minejar.exists())
-{
-	showButton(minejar.getAbsolutePath() + " not found - bailing out")
-}
-loader.addURL(minejar.toURI().toURL())
+///////////////////////////////////////////////
+// run the script
+///////////////////////////////////////////////
 
-// def host = new ss.SexScriptsHost(this);
-def Host = Class.forName('teaselib.hosts.SexScriptsHost', true, loader)
-def host = Host.newInstance(this)
-
-// def persistence = new ss.SexScriptsStatePersistence(this);
-def Persistence = Class.forName('teaselib.hosts.SexScriptsStatePersistence', true, loader)
-def persistence = Persistence.newInstance(this)
-
-// teaseLib = new teaselib.TeaseLib(
-def TeaseLib = Class.forName('teaselib.core.TeaseLib', true, loader)
-def teaseLib = TeaseLib.newInstance(host,persistence)
-
-//def mine = new Mine();
-def Mine = Class.forName('teaselib.scripts.mine.Mine', true, loader)
-def mine = Mine.newInstance(teaseLib, "/Mine/mistress/Vana/")
-mine.run()
+teaselib.core.TeaseLib.run(
+	new teaselib.hosts.SexScriptsHost(this),
+	new teaselib.hosts.SexScriptsStatePersistence(this),
+	scriptResources,
+	mainScriptClass);
