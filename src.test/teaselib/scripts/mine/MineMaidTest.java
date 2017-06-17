bb8510909608d57c7f70280fecb1c67344836d9a
Citizen-Cane <Citizen-Cane@users.noreply.github.com>
2017-06-17 17:24:59 +0200

    Test can perform random position and assert success

--- src.test/teaselib/scripts/mine/MineMaidTest.java
+++ src.test/teaselib/scripts/mine/MineMaidTest.java
@@ -1,21 +1,46 @@
 package teaselib.scripts.mine;
 
+import static org.junit.Assert.assertEquals;
+
 import java.io.File;
+import java.io.IOException;
 
 import org.junit.Test;
 
+import pcm.model.ActionRange;
+import pcm.model.ScriptExecutionException;
+import pcm.model.ScriptParsingException;
+import pcm.model.ValidationIssue;
+import pcm.state.persistence.ScriptState;
+import teaselib.core.Debugger;
 import teaselib.core.TeaseLib;
 import teaselib.hosts.DummyHost;
 import teaselib.hosts.DummyPersistence;
 
 public class MineMaidTest {
+    static final int ENABLE_MINE_MAID_DEBUGGING = 121;
 
     @Test
-    public void testActivity() {
-        TeaseLib teaseLib = new TeaseLib(new DummyHost(), new DummyPersistence());
+    public void testRandomPosition()
+            throws ScriptParsingException, ValidationIssue, ScriptExecutionException, IOException {
+        Debugger debugger = new Debugger(new DummyHost(), new DummyPersistence());
+
+        TeaseLib teaseLib = debugger.teaseLib;
+
         Mine mine = new Mine(teaseLib, new File("../SexScripts/scripts/"));
 
-        mine.play("mine-maid");
+        debugger.freezeTime();
+
+        debugger.addResponse("*spurted*", Debugger.Response.Ignore);
+        debugger.addResponse("*give*", Debugger.Response.Ignore);
+        debugger.addResponse("Exit", Debugger.Response.Choose);
+
+        mine.loadScript("mine-maid");
+        mine.state.set(ENABLE_MINE_MAID_DEBUGGING);
+        mine.play(new ActionRange(1000), new ActionRange(1000, 9950));
+
+        // Expect a good ending
+        assertEquals(ScriptState.SET, mine.state.get(9950));
     }
 
 }
