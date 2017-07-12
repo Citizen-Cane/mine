c63bff94836afa59bf2b1ea84ef1ca74fae65884
Citizen-Cane <Citizen-Cane@users.noreply.github.com>
2017-07-12 13:58:29 +0200

    Removed test code

--- src/teaselib/scripts/mine/Mine.java
+++ src/teaselib/scripts/mine/Mine.java
@@ -42,12 +42,10 @@ public class Mine extends Player {
     private static final String ResourcesRoot = "Mine";
     private static final String MainScript = "Mine";
 
-    private static final Actor MineMistress = new Actor("Mistress", "Miss,",
-            Voice.Gender.Female, Locale.UK, Actor.Key.DominantFemale,
-            Images.None);
+    private static final Actor MineMistress = new Actor("Mistress", "Miss,", Voice.Gender.Female, Locale.UK,
+            Actor.Key.DominantFemale, Images.None);
 
-    private static final String[] Assets = { "Mine Scripts.zip",
-            "Mine Resources.zip", "Mine Mistress.zip" };
+    private static final String[] Assets = { "Mine Scripts.zip", "Mine Resources.zip", "Mine Mistress.zip" };
 
     private static final String[] OptionalAssets = { "Mine Speech.zip" };
 
@@ -68,8 +66,7 @@ public class Mine extends Player {
 
     public static void main(String argv[]) {
         try {
-            recordVoices(MineMistress, MainScript, new File(argv[0]),
-                    ResourcesRoot, Assets);
+            recordVoices(MineMistress, MainScript, new File(argv[0]), ResourcesRoot, Assets);
         } catch (ScriptParsingException e) {
             logger.error(e.getMessage(), e);
         } catch (ValidationIssue e) {
@@ -95,8 +92,8 @@ public class Mine extends Player {
     }
 
     public Mine(TeaseLib teaseLib, File basePath, String mistressPath) {
-        super(teaseLib, new ResourceLoader(basePath, ResourcesRoot, Assets,
-                OptionalAssets), MineMistress, Namespace, mistressPath);
+        super(teaseLib, new ResourceLoader(basePath, ResourcesRoot, Assets, OptionalAssets), MineMistress, Namespace,
+                mistressPath);
         // resources.addAssets(OptionalAssets);
 
         mapToysToMultipleItems();
@@ -112,52 +109,38 @@ public class Mine extends Player {
                 new MappedScriptItemValue(363, items(Toys.Wrist_Restraints)));
         state.addScriptValueMapping(MappedScriptState.Global,
                 new MappedScriptItemValue(364, items(Toys.Ankle_Restraints)));
-        state.addScriptValueMapping(MappedScriptState.Global,
-                new MappedScriptItemValue(365, items(Toys.Collar)));
-        state.addScriptValueMapping(MappedScriptState.Global,
-                new MappedScriptItemValue(367, items(Toys.Gag)));
-        state.addScriptValueMapping(MappedScriptState.Global,
-                new MappedScriptItemValue(368, items(Toys.Buttplug)));
+        state.addScriptValueMapping(MappedScriptState.Global, new MappedScriptItemValue(365, items(Toys.Collar)));
+        state.addScriptValueMapping(MappedScriptState.Global, new MappedScriptItemValue(367, items(Toys.Gag)));
+        state.addScriptValueMapping(MappedScriptState.Global, new MappedScriptItemValue(368, items(Toys.Buttplug)));
         state.addScriptValueMapping(MappedScriptState.Global,
                 new MappedScriptItemValue(370, items(Toys.Spanking_Implement)));
         state.addScriptValueMapping(MappedScriptState.Global,
                 new MappedScriptItemValue(380, item(Toys.Chastity_Device)));
 
         Items dildos;
-        if (persistentEnum(Sexuality.Sex.class)
-                .value() == Sexuality.Sex.Female) {
+        if (persistentEnum(Sexuality.Sex.class).value() == Sexuality.Sex.Female) {
             dildos = items(Toys.Dildo).all(Features.Anal);
         } else {
             dildos = items(Toys.Dildo).all();
         }
-        state.addScriptValueMapping(MappedScriptState.Global,
-                new MappedScriptItemValue(388, dildos));
+        state.addScriptValueMapping(MappedScriptState.Global, new MappedScriptItemValue(388, dildos));
 
         state.addScriptValueMapping(MappedScriptState.Global,
-                new MappedScriptItemValue(389,
-                        items(Toys.Cockring).all(Features.Vibrating),
+                new MappedScriptItemValue(389, items(Toys.Cockring).all(Features.Vibrating),
                         items(Toys.VaginalInsert).all(Features.Vibrating),
-                        items(Toys.Vibrator).all(Toys.Vibrators.HandsFree),
-                        items(Toys.EStim_Device)));
+                        items(Toys.Vibrator).all(Toys.Vibrators.HandsFree), items(Toys.EStim_Device)));
     }
 
     private void mapToysToSingleItem() {
-        state.addScriptValueMapping(MappedScriptState.Global,
-                new MappedScriptItemValue(361, item(Toys.Nipple_Clamps)));
+        state.addScriptValueMapping(MappedScriptState.Global, new MappedScriptItemValue(361, item(Toys.Nipple_Clamps)));
         state.addScriptValueMapping(MappedScriptState.Global,
                 new MappedScriptItemValue(362, item(Household.Clothes_Pegs)));
-        state.addScriptValueMapping(MappedScriptState.Global,
-                new MappedScriptItemValue(366, item(Toys.Rope)));
-        state.addScriptValueMapping(MappedScriptState.Global,
-                new MappedScriptItemValue(360, item(Toys.Chains)));
-        state.addScriptValueMapping(MappedScriptState.Global,
-                new MappedScriptItemValue(382, item(Toys.Blindfold)));
-        state.addScriptValueMapping(MappedScriptState.Global,
-                new MappedScriptItemValue(384, item(Toys.Humbler)));
-        state.addScriptValueMapping(MappedScriptState.Global,
-                new MappedScriptItemValue(383, item(Toys.Enema_Kit)));
-        state.addScriptValueMapping(MappedScriptState.Global,
-                new MappedScriptItemValue(387, item(Toys.Pussy_Clamps)));
+        state.addScriptValueMapping(MappedScriptState.Global, new MappedScriptItemValue(366, item(Toys.Rope)));
+        state.addScriptValueMapping(MappedScriptState.Global, new MappedScriptItemValue(360, item(Toys.Chains)));
+        state.addScriptValueMapping(MappedScriptState.Global, new MappedScriptItemValue(382, item(Toys.Blindfold)));
+        state.addScriptValueMapping(MappedScriptState.Global, new MappedScriptItemValue(384, item(Toys.Humbler)));
+        state.addScriptValueMapping(MappedScriptState.Global, new MappedScriptItemValue(383, item(Toys.Enema_Kit)));
+        state.addScriptValueMapping(MappedScriptState.Global, new MappedScriptItemValue(387, item(Toys.Pussy_Clamps)));
         state.addScriptValueMapping(MappedScriptState.Global,
                 new MappedScriptItemValue(385, item(Toys.Ball_Stretcher)));
     }
@@ -168,59 +151,41 @@ public class Mine extends Player {
                 new MappedScriptItemValue(371, item(Household.Hairbrush)));
         state.addScriptValueMapping(MappedScriptState.Global,
                 new MappedScriptItemValue(372, item(Household.Wooden_Spoon)));
-        state.addScriptValueMapping(MappedScriptState.Global,
-                new MappedScriptItemValue(373, item(Household.Ruler)));
-        state.addScriptValueMapping(MappedScriptState.Global,
-                new MappedScriptItemValue(374, item(Toys.Enema_Bulb)));
+        state.addScriptValueMapping(MappedScriptState.Global, new MappedScriptItemValue(373, item(Household.Ruler)));
+        state.addScriptValueMapping(MappedScriptState.Global, new MappedScriptItemValue(374, item(Toys.Enema_Bulb)));
     }
 
     private void mapSelfReferencingBodyParts() {
         state.addScriptValueMapping(MappedScriptState.Global,
-                new MappedScriptStateValue.Indefinitely(20,
-                        state(Body.OnNipples), Body.OnNipples));
+                new MappedScriptStateValue.Indefinitely(20, state(Body.OnNipples), Body.OnNipples));
         state.addScriptValueMapping(MappedScriptState.Global,
-                new MappedScriptStateValue.Indefinitely(21, state(Body.InButt),
-                        Body.InButt));
+                new MappedScriptStateValue.Indefinitely(21, state(Body.InButt), Body.InButt));
         state.addScriptValueMapping(MappedScriptState.Global,
-                new MappedScriptStateValue.Indefinitely(22, state(Body.InMouth),
-                        Body.InMouth));
+                new MappedScriptStateValue.Indefinitely(22, state(Body.InMouth), Body.InMouth));
         state.addScriptValueMapping(MappedScriptState.Global,
-                new MappedScriptStateValue.Indefinitely(23, state(Body.OnPenis),
-                        Body.OnPenis));
+                new MappedScriptStateValue.Indefinitely(23, state(Body.OnPenis), Body.OnPenis));
         state.addScriptValueMapping(MappedScriptState.Global,
-                new MappedScriptStateValue.ForSession(24, state(Body.OnBalls),
-                        Body.OnBalls));
+                new MappedScriptStateValue.ForSession(24, state(Body.OnBalls), Body.OnBalls));
         state.addScriptValueMapping(MappedScriptState.Global,
-                new MappedScriptStateValue.ForSession(25,
-                        state(Body.AnklesTied), Body.AnklesTied));
+                new MappedScriptStateValue.ForSession(25, state(Body.AnklesTied), Body.AnklesTied));
         state.addScriptValueMapping(MappedScriptState.Global,
-                new MappedScriptStateValue.ForSession(26,
-                        state(Body.WristsTied), Body.WristsTied));
+                new MappedScriptStateValue.ForSession(26, state(Body.WristsTied), Body.WristsTied));
         state.addScriptValueMapping(MappedScriptState.Global,
-                new MappedScriptStateValue.ForSession(27,
-                        state(Body.AroundWaist), Body.AroundWaist));
+                new MappedScriptStateValue.ForSession(27, state(Body.AroundWaist), Body.AroundWaist));
         state.addScriptValueMapping(MappedScriptState.Global,
-                new MappedScriptStateValue.ForSession(28,
-                        state(Body.AroundNeck), Body.AroundNeck));
+                new MappedScriptStateValue.ForSession(28, state(Body.AroundNeck), Body.AroundNeck));
         state.addScriptValueMapping(MappedScriptState.Global,
-                new MappedScriptStateValue.ForSession(30,
-                        state(Body.CantJerkOff), Body.CantJerkOff));
+                new MappedScriptStateValue.ForSession(30, state(Body.CantJerkOff), Body.CantJerkOff));
         state.addScriptValueMapping(MappedScriptState.Global,
-                new MappedScriptStateValue.ForSession(31,
-                        state(Body.CantSitOnChair), Body.CantSitOnChair));
+                new MappedScriptStateValue.ForSession(31, state(Body.CantSitOnChair), Body.CantSitOnChair));
         state.addScriptValueMapping(MappedScriptState.Global,
-                new MappedScriptStateValue.ForSession(32, state(Body.CantStand),
-                        Body.CantStand));
+                new MappedScriptStateValue.ForSession(32, state(Body.CantStand), Body.CantStand));
         state.addScriptValueMapping(MappedScriptState.Global,
-                new MappedScriptStateValue.ForSession(33, state(Body.CantKneel),
-                        Body.CantKneel));
+                new MappedScriptStateValue.ForSession(33, state(Body.CantKneel), Body.CantKneel));
         state.addScriptValueMapping(MappedScriptState.Global,
-                new MappedScriptStateValue.ForSession(34,
-                        state(Body.CantTypeOrUseMouse),
-                        Body.CantTypeOrUseMouse));
+                new MappedScriptStateValue.ForSession(34, state(Body.CantTypeOrUseMouse), Body.CantTypeOrUseMouse));
         state.addScriptValueMapping(MappedScriptState.Global,
-                new MappedScriptStateValue.ForSession(35, state(Body.Tethered),
-                        Body.Tethered));
+                new MappedScriptStateValue.ForSession(35, state(Body.Tethered), Body.Tethered));
     }
 
     private void mapAssignments() {
@@ -229,8 +194,7 @@ public class Mine extends Player {
     }
 
     private void clearSessionTimerCheck() {
-        PersistentBoolean clearTimeCheck = persistentBoolean(
-                Assignments.ClearSessionTimerCheck);
+        PersistentBoolean clearTimeCheck = persistentBoolean(Assignments.ClearSessionTimerCheck);
         if (clearTimeCheck.isTrue()) {
             setScriptState(MainScript, 95, 0);
             clearTimeCheck.clear();
@@ -238,8 +202,7 @@ public class Mine extends Player {
     }
 
     private void triggerCarryOutEnema() {
-        PersistentBoolean triggerCarryOutEnema = persistentBoolean(
-                Assignments.CarryOutEnema);
+        PersistentBoolean triggerCarryOutEnema = persistentBoolean(Assignments.CarryOutEnema);
         if (triggerCarryOutEnema.isTrue()) {
             setScriptState(MainScript, 267, 1);
             triggerCarryOutEnema.clear();
@@ -253,58 +216,14 @@ public class Mine extends Player {
         Body[] peers = new Body[] { Body.OnPenis, Body.CantJerkOff };
 
         state.addScriptValueMapping(MappedScriptState.Global,
-                new MappedScriptStateValue.Indefinitely(chastified,
-                        chastityCage, peers));
+                new MappedScriptStateValue.Indefinitely(chastified, chastityCage, peers));
 
-        state.addStateTimeMapping(MappedScriptState.Global, haveKey,
-                chastityCage, peers);
+        state.addStateTimeMapping(MappedScriptState.Global, haveKey, chastityCage, peers);
     }
 
     @Override
     public void run() {
-
-        say("In main ");
-        reply(new ScriptFunction() {
-            @Override
-            public void run() {
-                say("Start of script function 1.");
-
-                say("Answer question level 1.");
-                reply("Yes Level 1", "No Level 1");
-
-                reply(new ScriptFunction() {
-                    @Override
-                    public void run() {
-                        say("Start of script function 2.");
-
-                        say("Answer question level 2.");
-                        reply("Wow Level 2", "Oh Level 2");
-
-                        reply(new ScriptFunction() {
-                            @Override
-                            public void run() {
-                                say("Start of script function 3.");
-
-                                say("Answer question level 3.");
-                                reply("No Level 3", "Wow Level 3",
-                                        "Oh Level 3");
-                                say("End of script function 3");
-
-                            }
-                        }, "Stop script function 3");
-
-                        say("End of script function 2");
-
-                    }
-                }, "Stop script function 2");
-
-                say("End of script function 1.");
-
-            }
-        }, "Stop script function 1");
-        say("Resuming main script");
-
-        // play(MainScript);
+        play(MainScript);
     }
 
     public void punish(Punishment punishment) {
